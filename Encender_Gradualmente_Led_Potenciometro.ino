8855c28e6e4f5740b852e2498e1f8728be87f93d
victorcubedo7 <45770234+victorcubedo7@users.noreply.github.com>
2018-12-12 16:13:53 +0100

    Add files via upload

---
+++ Encender_Gradualmente_Led_Potenciometro.ino
@@ -0,0 +1,20 @@
+int led = 3;  //¡¡¡Es necesario colocar el led en este pin!!!
+int poten = 0; //Potenciometro conectado a A0
+int brillo;
+
+
+void setup() {
+
+  pinMode(led, OUTPUT);
+  //El `potenciometro se declara automaticamente
+}
+
+void loop() {
+
+  brillo = analogRead(poten) / 4; //se divide entre 4 para poder usarlo 1023/4=255, que es hasta donde lee el analog Write
+
+  analogWrite(led, brillo);
+
+
+
+}
